2c8a1f24fc780a7069672b72c5b77868dcabb032
Tom Dirix <dirixtom@gmail.com>
2015-02-25 16:56:52 +0100

    Added cookie

--- 5 Login (Ello)/nav.inc.php
+++ 5 Login (Ello)/nav.inc.php
@@ -2,34 +2,49 @@
 	
 	function canLogin($p_username, $p_password)
 	{
-		// this function only checks if a user may or may not log in
+		if($p_username == "tom" && $p_password == "test")
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
 	}
 
 	function isLoggedIn()
 	{
-		// check if a user has previously logged in
+		if(isset($_COOKIE['loginCookie']))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 
 	function doLogin($p_user)
 	{
-		// this function sets the cookie required for subsequent logins
+		$content = $p_username;
+		setcookie("loginCookie", $content, time()+60*60);
 	}
 
-
-		
-	
-	
-
+	if(!empty($_POST))
+	{
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+	}
 ?>
 
 <nav>
-	
-	<!--<p class="welcome">Welcome back! <a href="logout.php">Log out?</a></p>-->
-			
+	<?php if( !isLoggedIn() ): ?>
 	<form action="" method="post">
 		<input class="input" type="text" name="username" placeholder="Your username">
 		<input class="input" type="password" name="password" placeholder="Your password">
 		<button class="button" type="submit">Log in</button>
 	</form>
-	
+	<?php else: ?>
+	<p class="welcome">Welcome back! <a href="logout.php">Log out?</a></p>
+	<?php endif; ?>
 </nav>
